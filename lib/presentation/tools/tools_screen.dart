099168b8d051fe3ed34a34df340b8e740b6300ac
aiartist-karl <1643143@qq.com>
2026-07-25 00:59:34 +0800

    Fix tools_screen.dart import path
    ../../core/config/app_config.dart → ../../config/app_config.dart

--- lib/presentation/tools/tools_screen.dart
+++ lib/presentation/tools/tools_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import '../../core/config/app_config.dart';
+import '../../config/app_config.dart';
 
 class ToolsScreen extends StatefulWidget {
   const ToolsScreen({super.key});
